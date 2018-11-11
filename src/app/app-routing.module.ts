a9503ba73ae9b3483128a632f7c0110145070169
ponta0929 <ponta.matumoto2@gmail.com>
2018-11-11 18:14:59 +0900

    VIdeoコンポーネントの作成、前準備

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { VideoComponent } from "./media/video/video.component";
+
+const routes: Routes = [
+  { path : "", component : VideoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
